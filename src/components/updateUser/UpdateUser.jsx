6a2e51e4cd9268edba41c620f3a7d6449f4ebb72
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-03-24 17:35:40 +0600

    create  update information form

--- src/components/updateUser/UpdateUser.jsx
+++ src/components/updateUser/UpdateUser.jsx
@@ -1,18 +1,23 @@
 import {useLoaderData} from 'react-router-dom'
 const UpdateUser = () => {
     const loadedUser = useLoaderData();
-
+    const updateUser = event =>{
+      event.preventDefault();
+      const name = event.target.name.value;
+      const email = event.target.email.value;
+      console.log(name, email);
+    }
   return (
     <div>
       <h2>Update </h2>
         <h3> Update user name : {loadedUser.name} and email: {loadedUser.email} </h3>
-      {/* <form onSubmit={handleAddUser}>
-        <input type="text" name="name" id="" />
+      <form onSubmit={updateUser} >
+        <input type="text" name="name" defaultValue={loadedUser.name} id="" />
         <br />
-        <input type="email" name="email" id="" />
+        <input type="email" defaultValue={loadedUser.email} name="email" id="" />
         <br />
-        <input type="submit" value="Add User" />
-      </form> */}
+        <input type="submit" value="Update User" />
+      </form>
     </div>
   );
 };
